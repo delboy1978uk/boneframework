cd4f4dac7323ca8c59e402b5bf96203eba3d355e
delboy1978uk <delboy1978uk@gmail.com>
2018-10-30 01:20:26 +0100

    mail server config in development folder

--- config/development/config.php
+++ config/development/config.php
@@ -5,9 +5,19 @@
  */
 return [
     'db' => [
-    'host' => '127.0.0.1',
-    'database' => '',
-    'user' => '',
-    'pass' => '',
+        'host' => '127.0.0.1',
+        'database' => '',
+        'user' => '',
+        'pass' => '',
+    ],
+    'mail' => [
+        'name'              => 'mail',
+        'host'              => 'mail',
+        'port'              => 1025,
+//        'connection_class'  => 'login', // plain, login, crammd5
+//        'connection_config' => [
+//            'username' => 'user',
+//            'password' => 'pass',
+//        ],
     ],
 ];
