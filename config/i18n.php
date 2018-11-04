fc0d7a733fbd40fece30d6193badc7ded18894ea
delboy1978uk <delboy1978uk@gmail.com>
2018-11-04 02:15:08 +0100

    tweak supported languages config

--- config/i18n.php
+++ config/i18n.php
@@ -5,6 +5,6 @@ return [
         'translations_dir' => 'data/translations',
         'type' => \Zend\I18n\Translator\Loader\Gettext::class,
         'default_locale' => 'en_GB',
-        'supported_locales' => ['en_GB', 'nl_BE', 'es_ES'],
+        'supported_locales' => ['en_GB', 'nl_BE', 'fr_BE'],
     ]
 ];
