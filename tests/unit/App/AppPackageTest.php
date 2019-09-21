4a14d9470dc9333739a22180a25384f2e434ecd2
Derek Stephen McLean <delboy1978uk@gmail.com>
2019-09-21 02:36:34 +0200

    tests 100%

--- tests/unit/App/AppPackageTest.php
+++ tests/unit/App/AppPackageTest.php
@@ -7,6 +7,7 @@ use Barnacle\Container;
 use Bone\Mvc\View\PlatesEngine;
 use BoneMvc\Module\App\AppPackage;
 use Codeception\TestCase\Test;
+use League\Route\Router;
 
 class AppPackageTest extends Test
 {
@@ -35,10 +36,14 @@ class AppPackageTest extends Test
     {
         $container = new Container();
         $view = new PlatesEngine('src/App/View');
+        $router = new Router();
         $container[PlatesEngine::class] = $view;
+        $container[Router::class] = $router;
         $this->assertFalse($this->package->hasEntityPath());
         $this->assertEmpty($this->package->getEntityPath());
         $this->package->addToContainer($container);
         $this->assertInstanceOf(IndexController::class, $container->get(IndexController::class));
+        $router = $this->package->addRoutes($container, $router);
+        $this->assertInstanceOf(Router::class, $router);
     }
 }
