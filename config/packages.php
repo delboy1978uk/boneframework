1290b8d9bbf1c0c202528ba96062cc5779342717
Derek Stephen McLean <delboy1978uk@gmail.com>
2019-09-21 20:52:42 +0200

    remove modules still registered in the config

--- config/packages.php
+++ config/packages.php
@@ -1,18 +1,10 @@
 <?php
 
 use BoneMvc\Module\App\AppPackage;
-use BoneMvc\Module\BoneMvcDoctrine\BoneMvcDoctrinePackage;
-use BoneMvc\Module\BoneMvcUser\BoneMvcUserPackage;
-use BoneMvc\Module\Mp\MpPackage;
-use Random\Developer\Jedi\JediPackage;
 
 return [
     'packages' => [
         AppPackage::class,
-        BoneMvcDoctrinePackage::class,
-        BoneMvcUserPackage::class,
-        JediPackage::class,
-        MpPackage::class,
     ],
     'viewFolder' => 'src/App/View'
 ];
