1ca5d97eb4fc031d8bb366068671ddf425d77e10
Derek Stephen McLean <delboy1978uk@gmail.com>
2019-09-27 00:36:32 +0200

    site info config

--- config/site.php
+++ config/site.php
@@ -4,5 +4,10 @@ return [
     'site' => [
         'title' => 'Bone MVC Framework',
         'domain' => 'awesome.scot',
+        'contactEmail' => 'abc@awesome.scot',
+        'serverEmail' => 'noreply@awesome.scot',
+        'company' => 'Pirates Inc.',
+        'address' => '1 Big Tree, Booty Island',
+        'logo' => '/img/emails/logo.png',
     ],
 ];
