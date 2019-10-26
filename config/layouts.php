6c6f954d02adef7e67703829c1cbe59001bf4ef2
Derek Stephen McLean <delboy1978uk@gmail.com>
2019-10-26 23:17:53 +0200

    add layout overrides for error pages

--- config/layouts.php
+++ config/layouts.php
@@ -5,4 +5,12 @@ return [
     'available_layouts' => [
         'layouts/bonemvc',
     ],
+    'error_pages' => [
+        'exception' => 'error/error',
+        401 => 'error/not-authorised',
+        403 => 'error/not-authorised',
+        404 => 'error/not-found',
+        405 => 'error/not-allowed',
+        500 => 'error/error',
+    ],
 ];
