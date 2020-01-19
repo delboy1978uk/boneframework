969617dffc45a7a61b6d80040952cfd3a54143e7
Derek Stephen McLean <delboy1978uk@gmail.com>
2020-01-19 19:22:08 +0100

    view override docs in config

--- config/views.php
+++ config/views.php
@@ -1,5 +1,9 @@
 <?php
 
+/*
+ *  You can override views from vendor packages
+ *  You should copy the vendor view folder into your src
+ */
 return [
     'views' => [
         'bonemvcuser' => 'src/App/View/bone-user',
