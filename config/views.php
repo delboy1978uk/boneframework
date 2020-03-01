45fa66371e9f5e6c058496f284820d10b955dad0
Derek Stephen McLean <delboy1978uk@gmail.com>
2020-03-01 17:02:59 +0100

    Using v3.5.8 of bone

--- config/views.php
+++ config/views.php
@@ -2,7 +2,7 @@
 
 /*
  *  You can override views from vendor packages
- *  You should copy the vendor view folder into your src
+ *  You should copy the vendor view folder into your src and tweak from there
  */
 return [
     'views' => [
