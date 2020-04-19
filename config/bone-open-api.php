545dcee7b33d00a9aef39a8f9d958e7a09ba7660
Derek Stephen McLean <delboy1978uk@gmail.com>
2020-04-20 00:03:47 +0200

    add swagger client config array

--- config/bone-open-api.php
+++ config/bone-open-api.php
@@ -1,5 +1,9 @@
 <?php
 
 return [
-    'docs' => 'data/docs/api.json'
+    'docs' => 'data/docs/api.json',
+    'swaggerClient' => [
+        'clientId' => 'Your swagger client id',
+        'clientSecret' => 'Your swagger client secret',
+    ],
 ];
