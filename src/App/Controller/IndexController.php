9508118c1ca941538eb416c2d898eea53dd1ed45
Derek Stephen McLean <delboy1978uk@gmail.com>
2020-04-19 15:39:32 +0200

    add bone-open-api annotations for site info

--- src/App/Controller/IndexController.php
+++ src/App/Controller/IndexController.php
@@ -9,10 +9,38 @@ use Psr\Http\Message\ServerRequestInterface;
 
 /**
  * Class IndexController
- *
- * If you need to create a constructor, edit the package to set up automatically via dependency injection
- *
+ * If you need to create a constructor, edit the package class to set it  up automatically via dependency injection
  * @package App\Controller
+ *
+ * The below annotations are for use with delboy1978uk/bone-open-api, youy can remove them if you wont be using it
+ * @OA\Info(
+ *     version="1.0.0",
+ *     title="Bone Framework API",
+ *     description="This be a swashbucklin' API."
+ * )
+ * @OA\ExternalDocumentation(
+ *     description="By delboy1978uk",
+ *     url="https://github.com/delboy1978uk/boneframework"
+ * )
+ * @OA\SecurityScheme(
+ *     type="oauth2",
+ *     name="bone-oauth2",
+ *     securityScheme="bone-oauth2",
+ *     @OA\Flow(
+ *         flow="authorizationCode",
+ *         authorizationUrl="https://awesome.scot/oauth2/authorize",
+ *         scopes={
+ *             "basic": "Access public API data",
+ *             "register-client": "Create and rewgistger clients",
+ *         }
+ *     )
+ * )
+ * @OA\SecurityScheme(
+ *     type="apiKey",
+ *     in="header",
+ *     securityScheme="api_key",
+ *     name="api_key"
+ * )
  */
 class IndexController extends Controller
 {
