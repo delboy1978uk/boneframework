5dcb3f2193d760b0f3d76973848f79a77e055dfb
Derek Stephen McLean <delboy1978uk@gmail.com>
2020-08-28 16:02:12 +0200

    Adding bone-pay config

---
+++ config/bone-pay.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'bone-pay' => [
+        'adapters' => [
+            'PayPal_Rest' => [
+                'clientId'  =>  '',
+                'secret'  =>  '',
+                'testMode'  =>  true,
+            ],
+            'WorldPay JSON' => [
+                'merchantId'  =>  '',
+                'serviceKey'  =>  '',
+                'clientKey'  =>  '',
+            ],
+        ],
+    ],
+];
