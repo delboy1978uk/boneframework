5c72fd8d039c16878dd44b87474483817b12bf18
Derek Stephen McLean <delboy1978uk@gmail.com>
2020-12-19 17:25:34 +0100

    tweak db config, removing deprecated stuff, add compos=er keywords

--- config/development/bone-db.php
+++ config/development/bone-db.php
@@ -1,20 +1,11 @@
 <?php
 
-ini_set('xdebug.var_display_max_depth', 10);
-ini_set('xdebug.var_display_max_children', 256);
-ini_set('xdebug.var_display_max_data', 1024);
-
-/**
- *  We be feckin' around with t'configuration
- */
 return [
     'db' => [
         'driver' => 'pdo_mysql',
         'host' => 'mariadb',
-        'database' => 'awesome',
         'dbname' => 'awesome',
         'user' => 'dbuser',
-        'pass' => '[123456]',
         'password' => '[123456]',
     ],
 ];
