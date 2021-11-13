bf3d5d417f1b2f4f93cb78d5896f06f579c7b797
Derek Stephen McLean <delboy1978uk@gmail.com>
2021-11-13 15:08:39 +0100

    uspdate main bone-db config file

--- config/bone-db.php
+++ config/bone-db.php
@@ -3,9 +3,8 @@ return [
     'db' => [
         'driver' => 'pdo_mysql',
         'host' => '127.0.0.1',
-        'database' => '',
         'dbname' => '',
         'user' => '',
-        'pass' => ''
+        'password' => ''
     ],
 ];
