71f28c92bb14011eaec1cbc128f4380676869d36
Derek Stephen McLean <delboy1978uk@gmail.com>
2022-10-09 12:18:01 +0200

    install bone user

--- config/packages.php
+++ config/packages.php
@@ -4,12 +4,18 @@ use Bone\App\AppPackage;
 use Bone\BoneDoctrine\BoneDoctrinePackage;
 use Bone\Mail\MailPackage;
 use Bone\Paseto\PasetoPackage;
+use Del\Person\PersonPackage;
+use Bone\User\BoneUserPackage;
+use Del\UserPackage;
 
 return [
     'packages' => [
         MailPackage::class,
         BoneDoctrinePackage::class,
         PasetoPackage::class,
+        PersonPackage::class,
+        UserPackage::class,
+        BoneUserPackage::class,
         AppPackage::class,
     ],
 ];
