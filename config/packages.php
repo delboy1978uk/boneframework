8f03fb2f1784d93741b1aca5a054cdb5b5723b32
Derek Stephen McLean <delboy1978uk@gmail.com>
2022-10-09 12:14:40 +0200

    install bone doctrine

--- config/packages.php
+++ config/packages.php
@@ -1,11 +1,13 @@
 <?php
 
 use Bone\App\AppPackage;
+use Bone\BoneDoctrine\BoneDoctrinePackage;
 use Bone\Mail\MailPackage;
 
 return [
     'packages' => [
         MailPackage::class,
+        BoneDoctrinePackage::class,
         AppPackage::class,
     ],
 ];
