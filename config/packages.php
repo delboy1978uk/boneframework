7695f726a9f30a23d0072101412aaf73c1d1b587
Derek Stephen McLean <delboy1978uk@gmail.com>
2022-10-09 12:16:01 +0200

    add bone paseto

--- config/packages.php
+++ config/packages.php
@@ -3,11 +3,13 @@
 use Bone\App\AppPackage;
 use Bone\BoneDoctrine\BoneDoctrinePackage;
 use Bone\Mail\MailPackage;
+use Bone\Paseto\PasetoPackage;
 
 return [
     'packages' => [
         MailPackage::class,
         BoneDoctrinePackage::class,
+        PasetoPackage::class,
         AppPackage::class,
     ],
 ];
