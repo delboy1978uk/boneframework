71d5d6ee195aa092789f460dc838c85a38c4059f
Derek Stephen McLean <delboy1978uk@gmail.com>
2022-11-21 00:00:05 +0100

    update bone-native settings to include pkce code verifier

--- config/bone-native.php
+++ config/bone-native.php
@@ -2,7 +2,8 @@
 
 return [
     'bone-native' => [
-        'deepLink' => 'exp://10.10.1.60:19000/--/',
+        'deepLink' => 'exp://192.168.0.204:19000/--/',
+        'verifier' => 'RaYFJonXO4pxNh7w5ygj90D8ruMPvwigU.BPMNp.O9~-oym396zbL0Lf9wGPPJSWkg1Lrz.4rHgbn_XwoyapfcTfXIONjhlOcA78y11PQIt1ueFlCbJ6DZ892G3HpLkt' /** @see https://tonyxu-io.github.io/pkce-generator/ */
     ],
 ];
 
